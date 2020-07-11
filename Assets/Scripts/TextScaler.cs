eadb1b23e3eb23f222b49186c2e414f629355f3e
IsBebs <banangurkkungen@gmail.com>
2020-07-11 15:48:05 +0200

    Added rotation and scaling on menu buttons

---
+++ Assets/Scripts/TextScaler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextScaler : MonoBehaviour
+{
+    RectTransform rectTransform;
+    Vector2 originalScale;
+    [SerializeField]
+    float maxScale;
+    [SerializeField]
+    float ScaleTime;
+    [SerializeField]
+    Vector2 ScaleOffset;
+    
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        originalScale = rectTransform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scaleX = originalScale.x + maxScale * Mathf.Sin(Time.time * ScaleTime + ScaleOffset.x);
+        float scaleY = originalScale.y + maxScale * Mathf.Sin(Time.time * ScaleTime + ScaleOffset.y);
+        rectTransform.localScale = new Vector3(scaleX, scaleY, rectTransform.localScale.z);
+    }
+}
