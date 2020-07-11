bdc345295f0640080da0c238cee7a1b1a04d2dbb
IsBebs <banangurkkungen@gmail.com>
2020-07-11 08:56:29 +0200

    added gesse movement script, attractor and detractor

---
+++ Assets/Scenes/Bebs/GesseMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class GesseMovement : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody;
+    [SerializeField]
+    float maxSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (rigidbody.velocity.sqrMagnitude > maxSpeed * maxSpeed)
+        {
+            float magnitude = rigidbody.velocity.magnitude;
+            float deltaSpeed = magnitude - maxSpeed;
+            rigidbody.velocity = Vector2.ClampMagnitude( rigidbody.velocity,maxSpeed);
+        }
+    }
+
+    public void Addforce(Vector2 force)
+    {
+        rigidbody.AddForce(force);
+    }
+}
